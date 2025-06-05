eedac2cc2df02aaa068b52aae94649008eb881f1
ChaoDong12 <dongchao1107@gmail.com>
2025-06-05 12:33:56 +0100

    git note update

--- git.sh
+++ git.sh
@@ -9,3 +9,10 @@ git remote add origin git@github.com:ChaoDong12/Machine_Learning.git
 git push --set-upstream origin master
 git add .
 git commit -m "Initial commit"
+
+# After you revise some files:
+git add .
+git commit -m "Update files"
+git push origin                       # Push changes to the remote repository
+
+# 
