20f93cd15cc45f06c522f298d9ea5289ee370907
ChaoDong12 <dongchao1107@gmail.com>
2025-06-05 12:31:30 +0100

    git note update

--- git.sh
+++ git.sh
@@ -2,3 +2,10 @@
 ssh-keygen -t rsa
 ls ~/.ssh/
 cat ~/.ssh/id_rsa.pub
+
+ssh -T git@github.com 
+
+git remote add origin git@github.com:ChaoDong12/Machine_Learning.git
+git push --set-upstream origin master
+git add .
+git commit -m "Initial commit"
